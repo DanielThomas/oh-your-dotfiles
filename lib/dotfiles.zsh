a0466c85c9183c396b2adfd2ac72f97db7b63a1b
Danny Thomas <dannyt@netflix.com>
2018-06-01 14:05:48 -0700

    Copy realpath to dotfiles script

--- lib/dotfiles.zsh
+++ lib/dotfiles.zsh
@@ -1,4 +1,17 @@
 # install/update/reload
+function realpath() {
+  OURPWD=$PWD
+  cd "$(dirname "$1")"
+  LINK=$(readlink "$(basename "$1")")
+  while [ "$LINK" ]; do
+    cd "$(dirname "$LINK")"
+    LINK=$(readlink "$(basename "$1")")
+  done
+  REALPATH="$PWD/$(basename "$1")"
+  cd "$OURPWD"
+  echo "$REALPATH"
+}
+
 local defaults=$(realpath "${0:a:h}/../defaults")
 
 function dotfiles_install() {
