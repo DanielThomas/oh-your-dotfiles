22a289b9af24981005aa8c180444b5d0969a13ef
Danny Thomas <dannyt@netflix.com>
2018-06-07 09:22:37 -0700

    Fix extra space in brew upgrade output

--- lib/homebrew.zsh
+++ lib/homebrew.zsh
@@ -62,7 +62,7 @@ function brew_upgrade() {
     brew="brew $1"
     outdated=$(eval $brew outdated | sed -e :a -e '$!N; s/\n/, /; ta')
     if [ -n "$outdated" ]; then
-      run "upgrading homebrew $1 ($outdated)" "$brew upgrade"
+      run "upgrading $brew ($outdated)" "$brew upgrade"
       run "cleaning up homebrew $1" "$brew cleanup"
     fi
   fi
