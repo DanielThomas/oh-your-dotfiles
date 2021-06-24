2dfe1197717168547fc9067738f5cc5103e3cfc0
Danny Thomas <dannyt@netflix.com>
2021-06-24 12:34:46 +1000

    Allow updating a origin for a git repo and the second line to provide a push url

--- lib/git.zsh
+++ lib/git.zsh
@@ -1,25 +1,30 @@
 git_clone_or_pull() {
   if [ -d $2 ]; then
-    git_pull $2
+    git_pull $1 $2
   else
     git_clone $1 $2
   fi
 }
 
 git_clone() {
-  repo=$(head -n 1 $1)
+  fetch=$(head -n 1 $1)
+  push=$(tail -n 2 $1)
   dest=$2
-  if ! git clone --quiet $repo $dest; then
-    fail "clone for $repo failed"
+
+  if ! git -C "$dest" clone --quiet $fetch $dest; then
+    fail "clone for $fetch failed"
+  fi
+  if [ "$fetch" != "$push" ]; then
+    git -C "$dest" remote set-url origin --push $pushd
   fi
 
-  success "cloned $repo to `basename $dest`"
+  success "cloned $fetch to `basename $dest`"
 
   dir=$(dirname $1)
   base=$(basename ${1%.*})
   for patch in $(find $dir -maxdepth 2 -name $base\*.gitpatch); do
     pushd $dest >> /dev/null
-    if ! git am --quiet $patch; then
+    if ! git -C "$dest" am --quiet $patch; then
       fail "apply patch failed"
     fi
 
@@ -29,10 +34,17 @@ git_clone() {
 }
 
 function git_pull() {
-  pushd $1 > /dev/null
-  if ! git pull origin master --rebase --quiet; then
+  fetch=$(head -n 1 $1)
+  push=$(tail -n 2 $1)
+  dest=$2
+
+  git -C "$dest" remote set-url origin "$fetch"
+  if [ "$fetch" != "$push" ]; then
+    git -C "$dest" remote set-url origin --push $pushd
+  fi
+
+  if ! git -C "$dest" pull origin master --rebase --quiet; then
     fail "could not update $1"
   fi
   success "updated $1"
-  popd >> /dev/null
 }
