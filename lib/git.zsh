88a2c9341f9cf6c28ab403acc6e73dd325fd2cac
Danny Thomas <dannyt@netflix.com>
2022-10-11 16:55:08 +1100

    Add branch to repository update message

--- lib/git.zsh
+++ lib/git.zsh
@@ -44,11 +44,12 @@ function git_pull() {
   fi
 
   current_sha=$(git -C "$dest" rev-parse --short HEAD)
-  if ! git -C "$dest" pull origin $(git -C "$dest" remote show origin | grep 'HEAD branch' | sed 's/.*: //') --rebase --quiet; then
+  branch=$(git -C "$dest" remote show origin | grep 'HEAD branch' | sed 's/.*: //')
+  if ! git -C "$dest" pull origin "$branch" --rebase --quiet; then
     fail "could not update $1"
   fi
   new_sha=$(git -C "$dest" rev-parse --short HEAD)
   if [ "$current_sha" != "$new_sha" ]; then
-    success "updated $1 from $current_sha to $new_sha"
+    success "updated $1 from $current_sha to $new_sha (branch $branch)"
   fi
 }
