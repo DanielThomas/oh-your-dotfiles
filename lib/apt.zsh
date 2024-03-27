8cbe53cb8c35504d1fddc44a9de4c6408f1422a4
Danny Thomas <dannyt@netflix.com>
2024-03-27 15:56:59 +1100

    Improve logic

--- lib/apt.zsh
+++ lib/apt.zsh
@@ -3,11 +3,11 @@ apt_install_upgrade() {
   	return
   fi
   run "updating apt indexes" "sudo apt update"
-  packages=$(dotfiles_find_installer install.apt)
-  if [[ -n "$packages" ]]; then
-	for file in "$packages"; do
-		for package in $(cat "$packages"); do
-			if dpkg -l "$package" > /dev/null; then
+  package_files=$(dotfiles_find_installer install.apt)
+  if [[ -n "$package_files" ]]; then
+	for file in "$package_files"; do
+		for package in $(cat "$package_files"); do
+			if ! dpkg -l "$package" > /dev/null; then
 				run "installing $package" "sudo apt --yes install $(package)"
 			fi
 		done
