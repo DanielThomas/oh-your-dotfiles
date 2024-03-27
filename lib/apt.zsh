ac4db8e7d0e53c6575f049c0dd602bdbc92fb9dd
Danny Thomas <dannyt@netflix.com>
2024-03-27 15:41:28 +1100

    Improve apt support

--- lib/apt.zsh
+++ lib/apt.zsh
@@ -5,7 +5,11 @@ apt_install_upgrade() {
   packages=$(dotfiles_find_installer install.apt)
   if [[ -n "$packages" ]]; then
   	sudo apt update
-  	sudo apt install $(packages)
+	for file in "$packages"; do
+		for package in $(cat $packages); do
+			sudo apt install $(package)
+		done
+    done
   	sudo apt upgrade
   fi
 }
