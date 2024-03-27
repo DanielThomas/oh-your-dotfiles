980c79fb8f66b00e514442b85436f8d5041235a5
Danny Thomas <dannyt@netflix.com>
2024-03-27 15:49:56 +1100

    More better

--- lib/apt.zsh
+++ lib/apt.zsh
@@ -2,18 +2,16 @@ apt_install_upgrade() {
   if [[ "Linux" != "$(uname)" ]]; then
   	return
   fi
+  run "updating apt indexes" "sudo apt update"
   packages=$(dotfiles_find_installer install.apt)
   if [[ -n "$packages" ]]; then
-  	sudo apt update
 	for file in "$packages"; do
 		for package in $(cat "$packages"); do
 			if dpkg -l "$package" > /dev/null; then
-				run "installing $package"
-				sudo apt install $(package)
+				run "installing $package" "sudo apt install $(package)"
 			fi
 		done
     done
   fi
-  run "upgrading system packages"
-  sudo apt upgrade
+  run "upgrading system packages" "sudo apt upgrade"
 }
