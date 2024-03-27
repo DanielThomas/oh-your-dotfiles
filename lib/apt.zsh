58fb0a619b14d1b7f492b1ae632b7f9266398768
Danny Thomas <dannyt@netflix.com>
2024-03-27 15:46:38 +1100

    Improve apt some more

--- lib/apt.zsh
+++ lib/apt.zsh
@@ -1,13 +1,16 @@
 apt_install_upgrade() {
-  if [[ "Linux" == "$(uname)" ]]; then
+  if [[ "Linux" != "$(uname)" ]]; then
   	return
   fi
   packages=$(dotfiles_find_installer install.apt)
   if [[ -n "$packages" ]]; then
   	sudo apt update
 	for file in "$packages"; do
-		for package in $(cat $packages); do
-			sudo apt install $(package)
+		for package in $(cat "$packages"); do
+			if dpkg -l "$package" > /dev/null; then
+				run "installing $package"
+				sudo apt install $(package)
+			fi
 		done
     done
   	sudo apt upgrade
