14ae4c8249575ddc3344030dea5976dfb489e6ee
Danny Thomas <dannyt@netflix.com>
2024-03-28 11:02:28 +1100

    Make apt update conditional

--- lib/apt.zsh
+++ lib/apt.zsh
@@ -2,9 +2,9 @@ apt_install_upgrade() {
   if [[ "Linux" != "$(uname)" ]]; then
     return
   fi
-  run "updating apt indexes" "sudo apt update"
   package_files=$(dotfiles_find_installer install.apt)
   if [[ -n "$package_files" ]]; then
+    run "updating apt indexes" "sudo apt update"
     for file in "$package_files"; do
         for package in $(cat "$package_files"); do
             if ! dpkg -s "$package" 1> /dev/null 2>& 1; then
