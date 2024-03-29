70f4d592441af983e80e0eeaa557aa350cf0deae
Danny Thomas <dannyt@netflix.com>
2024-03-29 14:26:11 +1100

    Fix multiple apt file handling

--- lib/apt.zsh
+++ lib/apt.zsh
@@ -5,7 +5,7 @@ apt_install_upgrade() {
   package_files=$(dotfiles_find_installer install.apt)
   if [[ -n "$package_files" ]]; then
     run "updating apt indexes" "sudo apt update"
-    for file in "$package_files"; do
+    for file in $package_files; do
         for package in $(cat "$package_files"); do
             if ! dpkg -s "$package" 1> /dev/null 2>& 1; then
                 run "installing $package" "sudo apt --yes install $package"
