547d892ca9fcf0ada075d628c51102e3d175748d
Danny Thomas <dannyt@netflix.com>
2024-04-02 12:23:46 +1100

    Support apt upgrades

--- lib/apt.zsh
+++ lib/apt.zsh
@@ -3,10 +3,14 @@ apt_install_upgrade() {
     return
   fi
   run "updating apt indexes" "sudo apt update"
+  local apt_upgradable=$(apt list --upgradeable)
   for aptfile in `dotfiles_find_installer install.apt`; do
     for package in $(cat "$aptfile"); do
       if ! dpkg -s "$package" 1> /dev/null 2>& 1; then
         run "installing $package" "sudo apt --yes install $package"
+      fi
+      if echo "$apt_upgradable" | grep "^${package}/" > /dev/null; then
+        run "upgrading $package" "sudo apt --yes install $package"
       fi
     done
   done
