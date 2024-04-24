4584ecb48a38c9139d65c62e04f925757a208685
Danny Thomas <dannyt@netflix.com>
2024-04-24 19:31:05 +1000

    Avoid duplicate path separators

--- defaults/path.zsh
+++ defaults/path.zsh
@@ -1,15 +1,17 @@
 source "${0:a:h}/homebrew.zsh"
 BREW_PATH=""
 if [[ ":$PATH:" != *":$HOMEBREW_PREFIX/bin:"* ]]; then
-  BREW_PATH="$BREW_PATH:$HOMEBREW_PREFIX/bin"
+  BREW_PATH="${HOMEBREW_PREFIX}/bin:"
 fi
 if [[ ":$PATH:" != *":$HOMEBREW_PREFIX/sbin:"* ]]; then
-  BREW_PATH="$BREW_PATH:$HOMEBREW_PREFIX/sbin"
+  BREW_PATH="${BREW_PATH}${HOMEBREW_PREFIX}/sbin:"
 fi
 if [[ ":$PATH:" != *":/usr/local/bin:"* ]]; then
-  BREW_PATH="$BREW_PATH:/usr/local/bin"
+  BREW_PATH="${BREW_PATH}/usr/local/bin:"
 fi
 if [[ ":$PATH:" != *":/usr/local/sbin:"* ]]; then
-  BREW_PATH="$BREW_PATH:/usr/local/sbin"
+  BREW_PATH="${BREW_PATH}/usr/local/sbin:"
 fi
-export PATH=$BREW_PATH:$PATH
+if [ ! -z "$BREW_PATH" ]; then
+  export PATH="${BREW_PATH}${PATH}"
+fi
