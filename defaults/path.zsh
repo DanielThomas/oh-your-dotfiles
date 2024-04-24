7a6635133bc032029495eb60d06f39066cc1bd70
Danny Thomas <dannyt@netflix.com>
2024-04-24 12:05:33 +1000

    Avoid duplicating homebrew prefixes on the path

--- defaults/path.zsh
+++ defaults/path.zsh
@@ -1,3 +1,15 @@
 source "${0:a:h}/homebrew.zsh"
-BREW_PATH="$HOMEBREW_PREFIX/bin:$HOMEBREW_PREFIX/sbin:/usr/local/bin:/usr/local/sbin"
+BREW_PATH=""
+if [[ ":$PATH:" != *":$HOMEBREW_PREFIX/bin:"* ]]; then
+  BREW_PATH="$BREW_PATH:$HOMEBREW_PREFIX/bin"
+fi
+if [[ ":$PATH:" != *":$HOMEBREW_PREFIX/sbin:"* ]]; then
+  BREW_PATH="$BREW_PATH:$HOMEBREW_PREFIX/sbin"
+fi
+if [[ ":$PATH:" != *":/usr/local/bin:"* ]]; then
+  BREW_PATH="$BREW_PATH:/usr/local/bin"
+fi
+if [[ ":$PATH:" != *":/usr/local/sbin:"* ]]; then
+  BREW_PATH="$BREW_PATH:/usr/local/sbin"
+fi
 export PATH=$BREW_PATH:$PATH
