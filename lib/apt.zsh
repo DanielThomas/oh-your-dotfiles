160a889fb67236920d425dcaf66a0c4a2b123852
Danny Thomas <dannyt@netflix.com>
2024-05-16 12:45:13 -0700

    Run single apt command

--- lib/apt.zsh
+++ lib/apt.zsh
@@ -4,14 +4,18 @@ apt_install_upgrade() {
   fi
   run "updating apt indexes" "sudo apt update"
   local apt_upgradable=$(apt list --upgradeable)
+  local apt_packages=()
   for aptfile in `dotfiles_find_installer install.apt`; do
     for package in $(cat "$aptfile"); do
       if ! dpkg -s "$package" 1> /dev/null 2>& 1; then
-        run "installing $package" "sudo apt --yes install $package"
+        apt_packages+="$package"
       fi
       if echo "$apt_upgradable" | grep "^${package}/" > /dev/null; then
-        run "upgrading $package" "sudo apt --yes install $package"
+        apt_packages+="$package"
       fi
     done
   done
+  if [ ${#apt_packages[@]} -gt 0 ]; then
+    run "installing ${apt_packages[*]}" "sudo apt --yes install ${apt_packages[*]}"
+  fi
 }
